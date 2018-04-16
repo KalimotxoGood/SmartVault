a62794e92bab82c664e4f8d24cf7d27fabfc31da
jwalker884 <jww193@humboldt.edu>
2018-04-16 16:29:51 -0700

    committing non versioned code.

---
+++ SmartVaultProject/app/src/main/java/com/example/jww193/smartvaultapp2/PasswordActivity.java
@@ -0,0 +1,133 @@
+package com.example.jww193.smartvaultapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.View;
+import android.os.Bundle;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class PasswordActivity extends AppCompatActivity {
+    Button openVaultButton;
+    Button closeVaultButton;
+    String valFromMain;
+    EditText vaultPassField;
+    TextView userText;
+    String strURL = "http://www.Google.com";
+    String result="";
+
+    private static final String TAG = "Testing: ";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.password_main);
+        VaultOpen();
+
+        new ControlGPIO().execute();
+    }
+
+    public void VaultOpen() {
+        openVaultButton = (Button) findViewById(R.id.openVault);
+        closeVaultButton = (Button) findViewById(R.id.closeVault);
+        userText = (TextView) findViewById(R.id.welcomeUser);
+        valFromMain = getIntent().getExtras().getString("value");
+        userText.setText("Welcome " + valFromMain + "!");
+        vaultPassField = (EditText) findViewById(R.id.vaultPassword);
+
+
+        // listens for the open button to be clicked, provided that the correct password has been inputted
+        // Once pressed, will display that the Vault has been opened, else if password was entered incorrectly
+        // will display that access was denied
+        openVaultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText theNet = findViewById(R.id.myNet);
+                String netAddress = theNet.getText().toString();
+                EditText mDevice = findViewById(R.id.dName);
+                String chosenDevice = mDevice.getText().toString();
+                strURL = netAddress + "/" + chosenDevice + "/on";
+
+                if (vaultPassField.getText().toString().equals("OPEN")) {
+                    Toast.makeText(getApplicationContext(), strURL, Toast.LENGTH_SHORT).show();
+
+                    new ControlGPIO().execute();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Access denied", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        // listens for the close button to be clicked
+        // Once pressed, will display that the Vault has been closed
+        closeVaultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ControlGPIO().execute();
+                EditText theNet = findViewById(R.id.myNet);
+                String netAddress = theNet.getText().toString();
+                String chosenDevice = userText.getText().toString();
+                strURL = netAddress + "/" + chosenDevice + "/off";
+                Toast.makeText(getApplicationContext(), "Vault has been closed!", Toast.LENGTH_SHORT).show();
+
+                new ControlGPIO().execute();
+            }
+        });
+    }
+
+    public class ControlGPIO extends AsyncTask<String,String,String>
+    {
+        @Override
+        protected void onPreExecute() {
+            Toast.makeText(getApplicationContext(), "hello preexecute", Toast.LENGTH_SHORT).show();
+            super.onPreExecute();
+        }
+        @Override
+        protected void onPostExecute(String s)
+        {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String...params){
+            try
+            {
+                Log.d(TAG, "Message");
+                Toast.makeText(getApplicationContext(), "hello url?", Toast.LENGTH_SHORT).show();
+                URL url = new URL(strURL);
+                HttpURLConnection con = (HttpURLConnection)url.openConnection();
+                con.setRequestMethod("GET");
+                con.connect();
+                Toast.makeText(getApplicationContext(), "hello after URL", Toast.LENGTH_SHORT).show();
+                BufferedReader bf = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                String value = bf.readLine();
+                System.out.println("result is " + value);
+                result = value;
+
+                MyTask 
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+            }
+            return result;
+        }
+
+        public class UserInput
+        {
+
+        }
+    }
+}
